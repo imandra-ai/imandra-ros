a8c58a3b3e4db36aa15f7a3bb1b1662b34a43452
KKostya <kostya@imandra.ai>
2022-01-17 19:49:19 +0100

    Cleanedup project

---
+++ imandrabot_ws/src/imandrabot/launch/robot_launch.py
@@ -0,0 +1,81 @@
+import os
+import pathlib
+import launch
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions.path_join_substitution import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from webots_ros2_driver.webots_launcher import WebotsLauncher
+
+
+def generate_launch_description():
+    package_dir = get_package_share_directory('imandrabot')
+    world = LaunchConfiguration('world')
+    robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'imandrabot.urdf')).read_text()
+    ros2_control_params = os.path.join(package_dir, 'resource', 'ros2_control.yml')
+    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+
+    webots = WebotsLauncher(
+        world=PathJoinSubstitution([package_dir, 'worlds', world])
+    )
+
+    controller_manager_timeout = ['--controller-manager-timeout', '50']
+    controller_manager_prefix = 'python.exe' if os.name == 'nt' else ''
+    diffdrive_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner.py',
+        output='screen',
+        prefix=controller_manager_prefix,
+        arguments=['diffdrive_controller'] + controller_manager_timeout,
+        parameters=[
+            {'use_sim_time': use_sim_time},
+        ],
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner.py',
+        output='screen',
+        prefix=controller_manager_prefix,
+        arguments=['joint_state_broadcaster'] + controller_manager_timeout,
+        parameters=[
+            {'use_sim_time': use_sim_time},
+        ],
+    )
+
+    webots_driver = Node(
+        package='webots_ros2_driver',
+        executable='driver',
+        output='screen',
+        parameters=[
+            {'robot_description': robot_description,
+             'use_sim_time': use_sim_time},
+            ros2_control_params
+        ],
+        remappings=[
+            ('/diffdrive_controller/cmd_vel_unstamped', '/cmd_vel'),
+        ]
+    )
+
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'world',
+            default_value='world.wbt',
+            description='Choose world'
+        ),
+        webots,
+        diffdrive_controller_spawner,
+        joint_state_broadcaster_spawner,
+        webots_driver,
+
+        # This action will kill all nodes once the Webots simulation has exited
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=webots,
+                on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
+            )
+        )
+    ])
---
+++ imandrabot_ws/src/imandrabot/setup.py
@@ -0,0 +1,53 @@
+"""webots_ros2_epuck package setup file."""
+
+from setuptools import setup
+
+
+package_name = 'imandrabot'
+data_files = []
+data_files.append(('share/ament_index/resource_index/packages', [
+    'resource/' + package_name
+]))
+data_files.append(('share/' + package_name + '/launch', [
+    'launch/robot_launch.py',
+]))
+data_files.append(('share/' + package_name + '/worlds', [
+    'worlds/world.wbt'
+]))
+data_files.append(('share/' + package_name + '/protos', [
+    'protos/imandrabot.proto'
+]))
+data_files.append(('share/' + package_name + '/resource', [
+    'resource/imandrabot.urdf',
+    'resource/ros2_control.yml'
+]))
+data_files.append(('share/' + package_name, [
+    'package.xml'
+]))
+
+
+setup(
+    name=package_name,
+    version='0.0.2',
+    packages=[package_name],
+    data_files=data_files,
+    install_requires=['setuptools', 'launch'],
+    zip_safe=True,
+    author='Konstantin Kanishchev',
+    author_email='kostya@imandra.ai',
+    maintainer='Imandra',
+    maintainer_email='skostya@imandra.ai',
+    keywords=['ROS', 'Webots', 'Robot', 'Imandra', 'Formal Verification'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Imandra verified ROS2 node',
+    license='TODO',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [],
+        'launch.frontend.launch_extension': ['launch_ros = launch_ros']
+    }
+)
