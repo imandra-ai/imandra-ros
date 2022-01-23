95b27ce01a0af8c68b86025f106cc9900ef298e5
KKostya <kostya@imandra.ai>
2022-01-23 19:59:27 +0100

    Update models to new cg

--- imandra_model/src-model/model_codec.ml
+++ imandra_model/src-model/model_codec.ml
@@ -1,27 +1,33 @@
-open Ros_messages_pp;;
-module J  = Yojson.Basic
-module JU = Yojson.Basic.Util 
+module D = Decoders_yojson.Basic.Decode
+module RD = Ros_messages_json.Decode(D)
+module E = Decoders_yojson.Basic.Encode
+module RE = Ros_messages_json.Encode(E)
 
 
 let json_to_incoming_opt json =
-  let data =  JU.member "data" json in
-  if data == `Null then None else
-  match JU.member "tag" json |> JU.to_string_option with
-  | Some "laser_scan" -> begin
-    match Sensor_msgs_of_json.json_to_laserScan_opt data with 
-    | Some data -> Some ( Ros_model.Sensor data ) | None -> None
-  end
-  | Some "clock" -> begin
-    match Rosgraph_msgs_of_json.json_to_clock_opt data with 
-    | Some data -> Some ( Ros_model.Clock data ) | None -> None
-  end
-  | _ -> None
+  let decoder : Ros_model.incoming_msg D.decoder =
+    let open D.Infix in
+    let* tag = D.field_opt "tag" D.string in
+    match tag with
+    | None -> D.fail "Missing tag field" 
+    | Some "clock" -> 
+      let* d = D.field "data" RD.Rosgraph_msgs.clock in
+      D.succeed @@ Ros_model.Clock d
+    | Some "laser_scan" -> 
+      let* d = D.field "data" RD.Sensor_msgs.laser_scan in
+      D.succeed @@ Ros_model.Sensor d
+    | Some s -> D.fail @@ "Unknown tag field " ^ s 
+    in
+  match D.decode_string decoder json with 
+  | Error e -> print_endline @@ D.string_of_error e; None
+  | Ok x -> Some x
+    
 
 
 let outgoing_to_json x =
-  match x with
-  | Ros_model.Twist data -> `Assoc 
-  [ ( "data" , data |> Geometry_msgs_to_json.twist_to_json )
-  ; ( "tag"  , `String "twist"    )
-  ]
+  let encoder : Ros_model.outgoing_msg E.encoder = function 
+    | Ros_model.Twist d -> E.obj 
+      [("tag", E.string "twist"); ("data", RE.Geometry_msgs.twist d)]
+    in
+  E.encode_string encoder x
   
